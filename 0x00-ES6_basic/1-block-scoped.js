13e6b62ba762a448121c49c32dafa4b2e29d354d
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-06-26 06:50:47 -0700

    modified variables in condition block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = true;
-  var task2 = false;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    var task = false;
+    var task2 = true;
   }
 
   return [task, task2];
