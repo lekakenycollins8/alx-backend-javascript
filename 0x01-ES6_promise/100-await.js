1578e4570af03eb6db84ac833b263208bc293510
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-06-28 07:35:20 -0700

    finished advanced task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from '../utils';
+
+export async function asyncUploadUser() {
+	try {
+		const photo = await uploadPhoto();
+		const user = await createUser();
+
+		return {photo: photo, user: user};
+	} catch(error) {
+		return({photo: null, user: null};
+	}
+}
+
+	
