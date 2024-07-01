7c45ce3c435dec340d5c59f891830f6c8c365c84
lekakenycollins8 <lekakenycollins8@gmail.com>
2024-07-01 16:48:18 +0300

    used filter and map to return list of students with grade from a particular city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity (list, city, newGrades) {
+    return list.filter((student) => student.location === city).map(
+        (student) => {
+            newGrades.map((studentGrade) => {
+                if (studentGrade.studentId === student.id) {
+                    student.grade = studentGrade.grade;
+                }
+                if (!student.hasOwnProperty('grade')) {
+                    student.grade = 'N/A';
+                }
+                return student;
+            });
+            return student;
+        });
+}
