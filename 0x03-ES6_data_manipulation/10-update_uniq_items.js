1a0cc31a85ac8449ed5d568b6d3e3da3ee9ab27a
lekakenycollins8 <lekakenycollins8@gmail.com>
2024-07-02 14:37:14 +0300

    made few corrections to various tasks

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (const [key, value] of map.entries()) {
+  for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
     }
