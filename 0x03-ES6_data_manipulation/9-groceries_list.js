b46da33471ea9c171a35249338208fe6d34607d2
lekakenycollins8 <lekakenycollins8@gmail.com>
2024-07-02 15:39:55 +0300

    corrected grocerieslist function

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,16 +1,9 @@
 export default function groceriesList() {
-    const finalMap = new Map();
-    const obj = {
-      Apples: 10,
-      Tomatoes: 10,
-      Pasta: 1,
-      Rice: 1,
-      Banana: 5,
-    };
-    //   get all the keys in the object into an array
-    const list = Array.from(Object.keys(obj));
-  
-    //   map through the array and for each item, set the value to the key in the finalMap
-    list.map((item) => finalMap.set(item, obj[item]));
-    return finalMap;
-  }
+    let map = new Map();
+    map.set('Apples', 10);
+    map.set('Tomatoes', 10);
+    map.set('Rice', 1);
+    map.set('Banana', 5);
+
+    return map;
+}
